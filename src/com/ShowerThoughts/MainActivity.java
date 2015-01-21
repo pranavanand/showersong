b2541850d7fc0ba6438e028e17d554303dcdcbaa
unknown <anandpranavv@gmail.com>
2015-01-20 20:58:59 -0800

    Added loopj.jar - very helpful - thank you Gurtham

--- src/com/ShowerThoughts/MainActivity.java
+++ src/com/ShowerThoughts/MainActivity.java
@@ -1,128 +1,132 @@
 package com.ShowerThoughts;
 
-import java.io.IOException;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Intent;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
-
-
 public class MainActivity extends ActionBarActivity {
-	
-	TextView firstSong;
-	TextView secondSong;
-	TextView thirdSong;
-	TextView fourthSong;
-	TextView fifthSong;
-	HttpClient client;
-	JSONObject json;
-	String url;
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		firstSong = (TextView) findViewById(R.id.textView1);
-		//secondSong = (TextView) findViewById(R.id.textView2);
-		//thirdSong = (TextView) findViewById(R.id.textView3);
-		//fourthSong = (TextView) findViewById(R.id.textView4);
-		//fifthSong = (TextView) findViewById(R.id.textView5);
-		
-		client = new DefaultHttpClient();
-		new Reader().execute("name");
-		
-		Intent intent = getIntent();
+
+    public static final String TAG = "CoDirMain";
+
+    private ListView mContactList;
+    private String[] seeds;
+    private AsyncHttpClient client;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Intent intent = getIntent();
 		Bundle bundle = intent.getExtras();
 		String username = bundle.getString("containsUsername");
 			
 		
 		url = "http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user="+username+"&api_key=68f82cd7b37e7b23800c2025066531c9&format=json";
-		//listOfSongs.loadUrl(url); http://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user="+username+"&api_key=68f82cd7b37e7b23800c2025066531c9&format=js
-		
-	}
-	
-	
-	
-	public String songCalled(int number) throws ClientProtocolException, IOException, JSONException {
-		StringBuilder URL = new StringBuilder(url);
-		HttpGet get = new HttpGet(URL.toString());
-		HttpResponse r = client.execute(get);
-		int status = r.getStatusLine().getStatusCode();
-		if (status == 200) {
-			HttpEntity e = r.getEntity();
-			String data = EntityUtils.toString(e);
-			JSONArray songList = new JSONArray(data);
-			JSONObject latest = songList.getJSONObject(number); 
-			String s = latest.getString("name");
-			return s; 
-		} else {
-			return null;
-		}
-		
-	}
-	
-	
-	
-	/*public class Read extends AsyncTask<String, Integer, String>{
-		
-		@Override
-		protected String doInBackground(String... params) {
-		try {
-				json = songCalled(0);
-				return json.getString(params[0]);
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-			
-			
-			return null;
-		}
-	} */
-	
-	protected void onPostExecute(String result) throws ClientProtocolException, IOException, JSONException {
-		//firstSong.setText
-		firstSong.setText(songCalled(0));
-	}
-	
-	
-	
-	
+        //mContactList = (ListView) findViewById(R.id.contact_list);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        client = new AsyncHttpClient();
+        client.get(url, new JsonHttpResponseHandler() {
+            ContactList adapter;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+            @Override
+            public void onStart() {
+                // called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+                JSONObject topTracks = null;
+                try {
+                    topTracks = response.getJSONObject("toptracks");
+                    JSONArray tracks = response.getJSONArray("track");
+                    
+
+                    Log.i(TAG, "Fetching contacts");
+
+                     = new String[results.length()]; // Basically IDs
+                    String[] fnames = new String[results.length()];
+                    String[] lnames = new String[results.length()];
+                    String[] cells  = new String[results.length()];
+                    String[] thumbs = new String[results.length()];
+
+                    for(int i = 0; i < results.length(); i++) {
+                        seeds[i]  = results.getJSONObject(i).getString("seed");
+                        fnames[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("name").getString("first");
+                        lnames[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("name").getString("last");
+                        cells[i]  = results.getJSONObject(i).getJSONObject("user").getString("cell");
+                        thumbs[i] = results.getJSONObject(i).getJSONObject("user").getJSONObject("picture").getString("thumbnail");
+                    }
+
+                    adapter = new ContactList(MainActivity.this, fnames, lnames, cells, thumbs);
+                    mContactList.setAdapter(adapter);
+                    mContactList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int p, long id) {
+                            final String seed = seeds[p];
+
+                            // Load Single Contact Activity
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    Log.e(TAG, "Failed to parse JSON. " + e.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.e(TAG, "Failed to get contacts.");
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                // called when request is retried
+                Log.e(TAG, "");
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+	
+	
+	
